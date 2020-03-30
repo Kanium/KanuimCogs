80217fcc4e9e6e2d34dd322898cb474d2542c420
Fadi AT <fadiatamny@gmail.com>
2020-03-30 14:32:48 +0300

    fetching format from jsonfile thats in github

--- welcomeCog/welcome.py
+++ welcomeCog/welcome.py
@@ -1,36 +1,37 @@
 import discord
 from discord.ext import commands
 import json
+import requests
 
-def fetchMessage():
-    with open('embedded_message.json') as file:
-        try:
-            jsonFormat = json.load(file)  
-            print(int(jsonFormat["color"],16))
-            message=discord.Embed(title=str(jsonFormat["title"]), description="".join(map(str, jsonFormat["description"])), color=int(jsonFormat["color"],16))      
-            message.set_thumbnail(url=jsonFormat["thumbnail"])
-            for field in jsonFormat["fields"]:
-                if(field["id"]!="links"):
-                    message.add_field(name=field["name"], value=field["value"], inline=field["inline"])
-                else:
-                    message.add_field(name=field["name"], value="".join(map(str,field["value"])), inline=field["inline"])
+embed = requests.get("https://raw.githubusercontent.com/Kanium/KanuimCogs/master/welcomeCog/embedded_message.json").text
 
-            message.set_footer(text=jsonFormat["footer"]["text"], icon_url=jsonFormat["footer"]["icon_url"])
-            return message
+def fetchMessage(jsonFormat):
+    try:
+        message=discord.Embed(title=str(jsonFormat['title']), description=''.join(map(str, jsonFormat['description'])), color=hex(jsonFormat['color']))      
+        message.set_thumbnail(url=jsonFormat['thumbnail'])
+        for field in jsonFormat['fields']:
+            if(field['id']!='links'):
+                message.add_field(name=field['name'], value=field['value'], inline=field['inline'])
+            else:
+                message.add_field(name=field['name'], value=''.join(map(str,field['value'])), inline=field['inline'])
 
-        except:
-            return "Welcome To Kanuim !"
+        message.set_footer(text=jsonFormat['footer']['text'], icon_url=jsonFormat['footer']['icon_url'])
+        return message
+
+    except:
+        return 'Welcome To Kanuim !'
 
 class WelcomeCog(commands.Cog):
     def __init__(self, bot):
-        self.message = fetchMessage()
+        self.message = json.load(embed)
         self.bot = bot
 
     @commands.Cog.listener()
     @commands.guild_only()
     async def on_member_join(self, member: discord.Member):
         try:
-            await member.send(content=None, embed=self.message)
+            message = fetchMessage(self.message)
+            await member.send(content=None, embed=message)
         except:
             print(
                 f'Error Occured! sending a dm to {member.display_name} didnt work !')
